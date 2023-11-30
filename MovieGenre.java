4ed80f1bc64d1949cc508b066c195555807dac38
Abil Damirbek uulu <116529783+abildamirbekuulu@users.noreply.github.com>
2023-11-29 22:52:49 -0800

    Create MovieGenre.java

---
+++ MovieGenre.java
@@ -0,0 +1,27 @@
+// Description: Creates a MovieGenre object with contruction, getter, and toString method
+
+import java.io.Serializable;
+
+public class MovieGenre implements Serializable {
+    // The serialVersionUID is used to verify compatibility of senders and
+    // receivers. See the document for more details:
+    // https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/io/Serializable.html
+    private static final long serialVersionUID = 205L; 
+    private String genre;
+    private String productionCompany;
+    //constructor
+    public MovieGenre(String genre, String productionCompany) {
+        this.genre = genre;
+        this.productionCompany = productionCompany;
+    }
+    //getter
+    public String getGenre() {
+        return genre;
+    }
+    //toString
+    @Override
+    public String toString() {
+        return genre + " Movie\n" +
+                "Production Company:\t" + productionCompany + '\n';
+    }
+}
